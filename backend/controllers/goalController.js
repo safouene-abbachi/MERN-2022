bf67f66c25954008b4280a8b83f65bdef0be0eaf
safouene <safouene-abbachi@hotmail.fr>
2022-02-24 15:19:50 +0100

    DEV : Initial REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,13 @@
 const asyncHandler = require('express-async-handler');
+
+const Goal = require('../model/goalModel');
 //@desc   Get goals
 //@route  GET/api/goals
 //@acess  private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get Goals' });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 //@desc   Set goals
 //@route  POST/api/goals
@@ -13,18 +17,36 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please add a text field');
   }
-  res.status(200).json({ message: 'Set goal' });
+  const goal = await Goal.create({ text: req.body.text });
+
+  res.status(200).json(goal);
 });
 //@desc   Update goal
 //@route PUT/api/goals/:id
 //@acess  private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 //@desc   Delete goals from
 //@route  DELETE/api/goals/:id
 //@acess  private
 const deleteGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('goal not found');
+  }
+  await goal.remove();
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middelware/errorMiddelware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-
+connectDB();
 const app = express();
-
-app.use('/api/goals', require('./routes/goalRoutes'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api/goals', require('./routes/goalRoutes'));
+
 // Override the express error handler by one of our one
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on ${port}`));
