49e300eccafc4134b07b7fc348946d54d4fce190
safouene <safouene-abbachi@hotmail.fr>
2022-03-10 17:53:44 +0100

    DEV : Login + register + JWT implementation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../model/userModel');
+
+//@desc   Register new User
+//@route  POST/api/users/
+//@acess  public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please complete all fields');
+  }
+
+  //Chheck if user exist
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create users
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      reqname: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+//@desc   Login User
+//@route  POST/api/users/login
+//@acess  public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  //Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      reqname: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
+});
+
+//@desc   Get User data
+//@route  GET/api/users/me
+//@acess  private
+const getMe = asyncHandler(async (req, res) => {
+  res.json({ message: 'User data dislplay' });
+});
+
+//Generate JWT
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+module.exports = { registerUser, loginUser, getMe };
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 // Override the express error handler by one of our one
 app.use(errorHandler);
